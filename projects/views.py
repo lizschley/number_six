df25fe1db8ef0e2372d5c5ab5d28e091d90e450d
lizschley <lizschley@gmail.com>
2020-04-24 15:02:17 -0400

    change order of projects

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,7 @@ from projects.models import Project
 
 # Create your views here.
 def all_projects(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all().order_by('id')
     print(request)
     return render(request, 'projects/all_projects.html', {'projects': projects})
 
