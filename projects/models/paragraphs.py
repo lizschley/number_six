df5dfc902e8e8be7aacbbf85b4ad30f22d94503a
lizschley <lizschley@gmail.com>
2020-06-16 15:13:02 -0400

    Creating explicit relationship between paragraphs and their references

--- projects/models/paragraphs.py
+++ projects/models/paragraphs.py
@@ -2,18 +2,21 @@ from django.db import models
 from autoslug import AutoSlugField
 
 
-class Group(models.Model):
-    title = models.CharField(max_length=120, blank=False, unique=True)
-    slug = AutoSlugField(unique=True, populate_from='title')
-    note = models.TextField(blank=True)
+class Reference(models.Model):
+    link_text = models.CharField(max_length=100, blank=False, unique=True)
+    slug = AutoSlugField(blank=False, unique=True, populate_from='link_text')
+    url = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __repr__(self):
-        return f'<Group id: {self.id}, title: {self.title}>'
+        return f'<Reference link_text: {self.link_text}>'
 
     def __str__(self):
-        return f'<Group id: {self.id}, title: {self.title}>'
+        return f'<Reference link_text: {self.link_text}>'
 
     class Meta:
+        verbose_name = 'reference'
         get_latest_by = 'updated_at'
 
 
@@ -24,13 +27,29 @@ class Paragraph(models.Model):
     standalone = models.BooleanField(default=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    groups = models.ManyToManyField(Group, through='GroupParagraph')
+    references = models.ManyToManyField(Reference, through='ParagraphReference')
 
     def __repr__(self):
         return f'<Paragraph id: {self.id}, standalone: {self.standalone}, subtitle== {self.subtitle}>'
 
     def __str__(self):
         return f'<Paragraph id: {self.id}, standalone: {self.standalone}, subtitle== {self.subtitle}>'
+
+    class Meta:
+        get_latest_by = 'updated_at'
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=120, blank=False, unique=True)
+    slug = AutoSlugField(unique=True, populate_from='title')
+    note = models.TextField(blank=True)
+    paragraphs = models.ManyToManyField(Paragraph, through='GroupParagraph')
+
+    def __repr__(self):
+        return f'<Group id: {self.id}, title: {self.title}>'
+
+    def __str__(self):
+        return f'<Group id: {self.id}, title: {self.title}>'
 
     class Meta:
         get_latest_by = 'updated_at'
@@ -50,21 +69,14 @@ class GroupParagraph(models.Model):
         return f'<Group id: {self.group}, para: {self.paragraph}, order: {self.order}>'
 
 
-class Reference(models.Model):
-    link_text = models.CharField(max_length=100, blank=False, unique=True)
-    slug = AutoSlugField(blank=False, unique=True, populate_from='link_text')
-    url = models.TextField(blank=False)
+class ParagraphReference(models.Model):
+    reference = models.ForeignKey(Reference, on_delete=models.CASCADE)
+    paragraph = models.ForeignKey(Paragraph, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    paragraphs = models.ManyToManyField(Paragraph)
 
     def __repr__(self):
-        return f'<Reference link_text: {self.link_text}>'
+        return f'<Reference id: {self.reference}, para: {self.paragraph}>'
 
     def __str__(self):
-        return f'<Reference link_text: {self.link_text}>'
-
-    class Meta:
-        verbose_name = 'reference'
-        get_latest_by = 'updated_at'
-
+        return f'<Reference id: {self.reference}, para: {self.paragraph}>'
