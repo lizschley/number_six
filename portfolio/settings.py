18a110804513aa5c69926e648fa03ab8f68ca64d
lizschley <lizschley@gmail.com>
2020-10-14 11:47:21 -0400

    add following to development

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('HASH_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'true' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
