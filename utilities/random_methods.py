0a074b035dc155f776ee9f08be231e7a65002bab
lizschley <lizschley@gmail.com>
2020-12-09 15:26:03 -0500

    Add convenience methods to make life easier

--- utilities/random_methods.py
+++ utilities/random_methods.py
@@ -1,5 +1,8 @@
 ''' Methods that are not specific to Paragraph functionality, but could be useful '''
+import os
+import shutil
 from django.utils.text import slugify
+import constants.file_paths as file_paths
 
 
 def valid_non_blank_string(str_to_check):
@@ -18,3 +21,55 @@ def valid_non_blank_string(str_to_check):
     except AttributeError:
         return False
     return not res
+
+
+def slugify_string(input_str):
+    '''
+    slugify_string slugifies the input string.  This is a convenience method, currently for creating
+    reference links for a new reference record in a new paragraph record.
+
+    :param input_str: string to slugify
+    :type input_str: str
+    :return: slugified version of the input string
+    :rtype: str
+    '''
+    return slugify(input_str)
+
+
+def archive_files_from_input_directories(include_done=True):
+    '''
+    archive_files_from_input_directories moves the files used for processing input data to
+    the archive location
+
+    :param include_done: whether to also move files that haven't been manually moved to done or loaded
+    :type include_done: bool, optional
+    '''
+    in_dirs = file_paths.ONLY_DONE_INPUT_DIRECTORIES
+    num_processed = 0
+    if include_done:
+        in_dirs += file_paths.NOT_DONE_INPUT_DIRECTORIES
+    for dir_path in in_dirs:
+        num_processed = loop_through_files_to_move(dir_path, num_processed)
+    print(f'After looping through the input data, {num_processed} files were moved.')
+
+
+def loop_through_files_to_move(input_dir_path, num_processed):
+    '''
+    loop_through_files_to_move moves all the files from input directory to output directory
+
+    :param input_dir_path: directory containing files to loop through, this is a shallow loop.
+    :type input_dir_path: str
+    :return: number of files that were moved
+    :rtype: int
+    '''
+    output_dir_path = file_paths.USED_INPUT_FINAL_DIRECTORY
+    for filename in os.listdir(input_dir_path):
+        if not filename.endswith('.json'):
+            continue
+        output_path = os.path.join(output_dir_path, filename)
+        input_path = os.path.join(input_dir_path, filename)
+        shutil.move(input_path, output_path)
+        num_processed += 1
+    return num_processed
+
+
