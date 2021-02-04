dcd61a58333da18a29dfff7932753331e4551b91
lizschley <lizschley@gmail.com>
2021-02-04 12:38:50 -0500

    Enable django 500 error email mailing to admins

---
+++ originals/sql/basic_counts.sql
@@ -0,0 +1,6 @@
+select count(*) as cat from projects_category;
+select count(*) as gr from projects_group;
+select count(*) as gp from projects_groupparagraph;
+select count(*) as para from projects_paragraph;
+select count(*) as pararef from projects_paragraphreference;
+select count(*) as project from projects_projects;
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -162,3 +162,11 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 # Trouble-shooting
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_REGION_NAME = 'us-east-1'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
