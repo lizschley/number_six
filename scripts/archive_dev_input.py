9e5844aa806a41b2ce35575a74a6e382fb1d795e
lizschley <lizschley@gmail.com>
2021-03-05 18:20:43 -0500

    Adding many resume groups and paras and a new flashcard set

--- scripts/archive_dev_input.py
+++ scripts/archive_dev_input.py
@@ -14,13 +14,13 @@ def run(*args):
         have not been moved to the done directory.
 
         The default, meaning no script args, archives even the input files that have not been
-        removed to the done folder.  This is currently a manual process, because of the
-        iterative nature of editing files or in case there was an error.
+        removed to the done folder.  Moving dev files to done is currently a manual process, because of
+        the iterative nature of editing files or in case there was an error.
 
         If you know there is an input file that you want to keep, just move it from the done file
         and run with the exclude_not_done argument
 
-        I do not archive production input, since the archive directory is not on the EC2 server
+        I do not archive production input, since the archive directory is not on the EC2 server.
 
         >>> python manage.py runscript -v3 s3_updater --script-args
         or
