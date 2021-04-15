82dab17dae4d986864f34ab443dab47dc99dfc81
lizschley <lizschley@gmail.com>
2021-04-15 17:54:21 -0400

    Initial subtheme

---
+++ originals/drupal/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.lizbarrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
