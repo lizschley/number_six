347f3b5194e2cfb2b11d43f146cde9161df7533e
lizschley <7227538+lizschley@users.noreply.github.com>
2025-09-20 11:15:02 -0400

    sort of working... needs more testing

--- common_classes/html_creator.py
+++ common_classes/html_creator.py
@@ -21,7 +21,8 @@ class HtmlCreator:
     def assign_data(self):
         self.input_data = self.build_input_data()
         pprint.pprint(self.input_data)
-        self.outfile = os.path.join(settings.BASE_DIR, self.input_data['files']['output_file'])
+        outfile = self.input_data['files']['output_file']
+        self.outfile = outfile if '/Users/eaffie/' in outfile else os.path.join(settings.BASE_DIR, outfile)
         self.infile = os.path.join(settings.BASE_DIR, self.input_data['files']['input_file'])
         print(f'Output file is {self.outfile}')
 
@@ -29,6 +30,8 @@ class HtmlCreator:
         input_data = {}
         input_data['files'] = page_data.FILES
         input_data['add_to_imports'] = self.additional_imports()
+        input_data['end_top_html'] = page_data.END_TOP_HTML
+        input_data['add_to_framework'] = self.additional_framework()
         input_data['end_html'] = page_data.END_HTML
         return input_data
 
@@ -40,9 +43,16 @@ class HtmlCreator:
             add_imports = add_imports + page_data.NEEDED_FOR_TABLE
         return add_imports
 
+    def additional_framework(self):
+        if page_data.FLAGS['accordian']:
+            return page_data.ACCORDIAN_HTML
+        return ''
+
     def create_html_file(self):
         utils.copy_file_from_source_to_target(self.infile, self.outfile)
         utils.append_file_from_array(self.input_data['add_to_imports'], self.outfile, 'a+')
+        utils.write_file_from_string(self.input_data['end_top_html'], self.outfile, 'a+')
+        utils.write_file_from_string(self.input_data['add_to_framework'], self.outfile, 'a+')
         utils.write_file_from_string(self.input_data['end_html'], self.outfile, 'a+')
 
     def append_additional_imports(self):
--- constants/html_page_data.py
+++ constants/html_page_data.py
@@ -4,21 +4,22 @@ import textwrap
 FLAGS = {
     'modal': False,
     'table': False,
+    'accordian': True,
 }
 
 FILES = {
     'input_file': 'basic_site_html/begin_html.html',
-    'output_file': 'basic_site_html/plant_overview.html',
+    'output_file': '/Users/eaffie/development/basic_website/liz/html/blog/test_using_blognbhn .html',
 }
 
 END_HTML_VARIABLES = {
-    'body_page_id': 'plant_overview_page_id',
-    'h1_id': 'plant_community_overview_h1',
-    'h1_default_text': 'Plant Community Overview',
-    'main_container_id': 'plant_community_main_container',
-    'h3_id': 'plant_community_overview_h3',
-    'h3_default_text': 'Plant Community Secondary Header',
-    'default_text': 'Plant Community default text',
+    'body_page_id': 'blog_intro_page_id',
+    'h1_id': 'blog_intro_overview_h1',
+    'h1_default_text': 'Blog Introduction',
+    'main_container_id': 'blog_intro_main_container',
+    'h3_id': 'blog_intro_h3',
+    'h3_default_text': 'Blog Introduction and Reason',
+    'default_text': 'Begin here',
 }
 
 # Pages with many modal options have separate data files
@@ -33,7 +34,7 @@ NEEDED_FOR_TABLE = [
     '<script src="../../js/plant_table.js" type="text/javascript" defer></script>',
 ]
 
-END_HTML = textwrap.dedent(f'''\
+END_TOP_HTML = textwrap.dedent(f'''\
         <title>Inquiries and Observations | Inquiries and Observations </title>
     </head>
     <body id='{END_HTML_VARIABLES['body_page_id']}'>
@@ -51,6 +52,38 @@ END_HTML = textwrap.dedent(f'''\
         <div id='{END_HTML_VARIABLES['main_container_id']}' class='container-fluid px-5'>
             <h3 id='{END_HTML_VARIABLES['h3_id']}'>{END_HTML_VARIABLES['h3_default_text']}</h3>
             <p>{END_HTML_VARIABLES['default_text']}</p>
-        </div>
+        </div>''')
+
+
+END_HTML = textwrap.dedent(f'''\
     </body>
 </html>''')
+
+ACCORDIAN_HTML = textwrap.dedent(f'''\
+        <div id='tech_questions_and_answers' class='container-fluid px-5'>
+            <h6>The following answers were accurate in late 2020:</h6>
+            <div class="accordion" id="tech-q-and-a-id">
+                <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    How do you add a Subdomain to a Letsencrypt Certificate?
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#tech-q-and-a-id">
+                    <div class="accordion-body">
+                    <p>
+                        When I accepted that in the real world you need to have a www subdomain, I was able to add it through AWS. I did not document that process, because AWS did a good job of explaining and it seemed to work perfectly. However, I later found out that it gave a security error in Safari. It said that my valid site may be spoofed. Therefore, I needed to add a www subdomain to my certificate.
+                    </p>
+                    <p>Ran this from <a href="https://technologytales.com/2019/06/11/adding-a-new-domain-or-subdomain-to-an-ssl-certificate-using-certbot/" class="reference_link" target="_blank">Technologytales 20190611</a>: </p>
+                    <pre><code>&gt;&gt;&gt;sudo certbot --expand -d existing.com,www.example.com</code></pre>
+                    It worked, but it broke the automatic redirect from www.domain.com to domain.com that worked before.  This is obviously an Apache problem, since www.domain.com brought up the default Apache page that lives on the server.
+                    <p></p>
+                    <p>This error was fixed by using advice from <a href="https://linuxize.com/post/redirect-http-to-https-in-apache/" class="reference_link" target="_blank">Linuxize 20200101</a>. I had to add their suggested code to two files (example.conf and example-le-ssl.conf). </p>
+                    <p>Everything works, which is great, but someday, I would like to look at what I have with someone who understands Apache best practices.  Plus, I should test the fix again after the cron job renews the certificate.</p>
+                    <h5>References</h5>
+                    <a href="https://technologytales.com/2019/06/11/adding-a-new-domain-or-subdomain-to-an-ssl-certificate-using-certbot/" class="reference_link" target="_blank">Technologytales_20190611_AddingNewDomainSubdomain_SSL_Certificate_Certbot</a><br><a href="https://linuxize.com/post/redirect-http-to-https-in-apache/" class="reference_link" target="_blank">Linuxize_20200101_RedirectHttpHttpsApache</a><br>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>''')
